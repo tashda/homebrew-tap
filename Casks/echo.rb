d39db933dbe13f4a4e42423097602eb303c84319
tashda <kenneth@skabel.se>
2026-03-10 19:10:09 +0100

    chore: update download URL and homepage to Echo repository

--- Casks/echo.rb
+++ Casks/echo.rb
@@ -2,10 +2,10 @@ cask "echo" do
   version "1.0,1"
   sha256 :no_check # This will be updated with the actual SHA256 of the release zip
 
-  url "https://github.com/tashda/fuzee/releases/download/v#{version.before_comma}-#{version.after_comma}/Echo.zip"
+  url "https://github.com/tashda/Echo/releases/download/v#{version.before_comma}-#{version.after_comma}/Echo.zip"
   name "Echo"
   desc "Database management and SQL editor"
-  homepage "https://github.com/tashda/fuzee"
+  homepage "https://github.com/tashda/Echo"
 
   # The --no-quarantine flag is recommended for users to bypass Gatekeeper
   # since the app is not notarized.
